7efd4479ad1736ef0f78caf54bf28e8983de4159
Khalil Ur Rehman <ali.rehman007@gmail.com>
2017-11-14 12:13:55 +0530

    First Commit

--- app/src/main/java/com/dupleit/mapmarkers/dynamicmapmarkers/MainActivity.java
+++ app/src/main/java/com/dupleit/mapmarkers/dynamicmapmarkers/MainActivity.java
@@ -3,11 +3,19 @@ package com.dupleit.mapmarkers.dynamicmapmarkers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
