d95bd513c85bec4925070a0568818796c45464c5
Khalil Ur Rehman <ali.rehman007@gmail.com>
2017-12-06 13:25:41 +0530

    update

---
+++ app/src/main/java/com/dupleit/mapmarkers/dynamicmapmarkers/Profile/ProfileActivity.java
@@ -0,0 +1,27 @@
+package com.dupleit.mapmarkers.dynamicmapmarkers.Profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.dupleit.mapmarkers.dynamicmapmarkers.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.UserProfileImage)
+    CircleImageView UserProfileImage;
+
+    @BindView(R.id.userEmail)
+    EditText email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+    }
+}
